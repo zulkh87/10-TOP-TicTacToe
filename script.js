c71f2ecd9b9eaad537d2dc31af55f26e0a9805f6
Zulkh87-AF-K <156850017+zulkh87@users.noreply.github.com>
2024-04-30 18:12:42 +0300

    restart button functionality added

--- script.js
+++ script.js
@@ -13,17 +13,24 @@ const WINNING_COMBINATIONS = [
 const cellElements = document.querySelectorAll("[data-cell]");
 const board = document.querySelector("#board");
 const winningMessageElement = document.getElementById("winningMessage");
+const restartButton = document.getElementById("restartButton");
 const winningMessageTextElement = document.querySelector("[data-winning-message-text]");
 let circleTurn;
 
 startGame();
 
+restartButton.addEventListener("click",startGame);
+
 function startGame(){
     circleTurn = false;
     cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(CIRCLE_CLASS);
+        cell.removeEventListener("click", handleClick);
         cell.addEventListener("click", handleClick, {once: true});
     });
     setBoardHoverClass();
+    winningMessageElement.classList.remove("show");
 }
 
 function handleClick(e){
